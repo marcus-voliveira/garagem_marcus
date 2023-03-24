c5cd25788e1c598c3a40c50fae8789db918c7f81
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-03-24 11:07:26 -0300

    Atualização 2

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "garagem_marcus"
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
