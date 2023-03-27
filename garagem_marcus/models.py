e93beb2ee3ed2b8cf686d68cd90ca794dc598268
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-03-27 16:33:58 -0300

    Commit passo 5

--- garagem_marcus/models.py
+++ garagem_marcus/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return self.nome.upper()                                 
