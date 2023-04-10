019263df303d402ac2611b5b306ea4c2be17e4e3
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-04-10 16:29:38 -0300

    Commit sei lá qual

--- garagem_marcus/admin.py
+++ garagem_marcus/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 
 from garagem_marcus.models import Marca
 from garagem_marcus.models import Categoria
+from garagem_marcus.models import Core
+from garagem_marcus.models import Acessorio
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Core)
+admin.site.register(Acessorio)
+
+
--- garagem_marcus/models.py
+++ garagem_marcus/models.py
@@ -12,5 +12,28 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.veiculo
+
+class Acessorio(models.Model):
+    descrição = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descrição()
     
-                                     
+class Core(models.Model):
+    descrição = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descrição()    
+    
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="Veiculos")
+    core = models.ForeignKey(Core, on_delete=models.PROTECT, related_name="Veiculos")
+    ano = models.CharField(default= 0, max_length=4, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
+    
+
+    def __str__(self):
+        return f"{self.marca} {self.categoria} {self.core} {self.ano}"    
+
+
