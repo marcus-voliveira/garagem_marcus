e4b3600bf7bef269a10b7cbd596088f7432352c2
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-07-28 08:45:42 -0300

    Terminando passo 13

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ),
 }
 
