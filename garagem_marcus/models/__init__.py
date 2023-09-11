cc257dcfae3e848243b73a4abb903fbacaa83e0a
Marcus <marcus.oliveira2403@gmail.com>
2023-09-11 15:48:38 -0300

    FEITO! - AMEM CARALHO

--- garagem_marcus/models/__init__.py
+++ garagem_marcus/models/__init__.py
@@ -2,5 +2,5 @@ from .cor import Cor
 from .marca import Marca
 from .acessorio import Acessorio
 from .categoria import Categoria
-from .veiculo import Veiculo
 from .modelo import Modelo
+from .veiculo import Veiculo
--- garagem_marcus/models/marca.py
+++ garagem_marcus/models/marca.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=255)
-    nacionalidade = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
