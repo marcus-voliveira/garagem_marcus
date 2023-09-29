e73439c9a753a3658fa49d1bd8303d02a9d5b2df
Marcus <marcus.oliveira2403@gmail.com>
2023-09-29 09:27:34 -0300

    Ainda tem erros pra corrigir

--- garagem_marcus/models/categoria.py
+++ garagem_marcus/models/categoria.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.descricao
--- garagem_marcus/serializers/veiculo.py
+++ garagem_marcus/serializers/veiculo.py
@@ -5,26 +5,26 @@ from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 class VeiculoSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(
-        source="capa",
+    imagem_attachment_key = SlugRelatedField(
+        source="imagem",
         queryset=Image.objects.all(),
         slug_field="attachment_key",
         required=False,
         write_only=True,
     )
-    capa = ImageSerializer(required=False, read_only=True)
+    imagem = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Veiculo
         fields = "__all__"
 
 class VeiculoDetailSerializer(ModelSerializer):
-    capa = ImageSerializer(required=False)
+    imagem = ImageSerializer(required=False)
     class Meta:
         model = Veiculo
         fields = "__all__"
-        depth = 1
+        depth = 1   
 
 class VeiculoListSerializer(ModelSerializer):
     class Meta:
         model = Veiculo
-        fields = ["id", "modelo", "ano"]
+        fields = ["id", "descricao", "ano"]
